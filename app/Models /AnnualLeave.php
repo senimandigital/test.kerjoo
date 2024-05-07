cff2b13902ac5aa735735b3a73ffe54395d27862
Eboy <eboy_karya_amatir@yahoo.co.id>
2024-05-07 21:48:04 +0700

    Create AnnualLeave.php

---
+++ app/Models /AnnualLeave.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AnnualLeave extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'tanggal_mulai',
+        'tanggal_berakhir',
+
+    ];
+}
